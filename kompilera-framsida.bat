0da3032ca27db248e34045c406ab6931f7332aac
Johan Winther <johwin@student.chalmers.se>
2017-07-10 16:33:52 +0200

    Kompilera utan rester

--- kompilera-framsida.bat
+++ kompilera-framsida.bat
@@ -1,6 +1,13 @@
 @echo off
-pdflatex.exe framsida.tex
+if not exist "tmp\sjungbok" mkdir tmp\sjungbok
+if not exist "tmp\sjungbok\bilder" mkdir tmp\sjungbok\bilder
+copy "framsida.tex" "tmp\sjungbok"
+copy "bilder\fmark.eps" "tmp\sjungbok\bilder"
+cd "tmp\sjungbok"
+pdflatex.exe -halt-on-error framsida.tex
+copy "framsida.pdf" "..\..\"
+cd ..\.
+@RD /S /Q sjungbok
+cd ..\.
 echo Klar!
 pause
-del framsida.aux
-del framsida.log
